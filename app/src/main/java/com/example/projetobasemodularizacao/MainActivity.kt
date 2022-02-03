5f2c858a839b42931299817c17e27d3f55272f57
joao <joao.ribeiro@havan.com.br>
2022-02-03 09:13:42 -0300

    Criando navegação entre app e comum

--- app/src/main/java/com/example/projetobasemodularizacao/MainActivity.kt
+++ app/src/main/java/com/example/projetobasemodularizacao/MainActivity.kt
@@ -2,12 +2,21 @@ package com.example.projetobasemodularizacao
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private val navController: NavController by lazy { findNavController(R.id.nav_host_fragment) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 
 }
---
+++ comum/src/main/java/com/example/comum/presentation/ui/LoginViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.comum.presentation.ui
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
