6636574258fa5c2c5db96036168e81cbcee3017c
joao <joao.ribeiro@havan.com.br>
2022-02-03 14:23:55 -0300

    create dependencies gradle

--- comum/src/main/java/com/example/comum/presentation/ui/LoginFragment.kt
+++ comum/src/main/java/com/example/comum/presentation/ui/LoginFragment.kt
@@ -6,15 +6,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputBinding
+import android.widget.Button
 import com.example.comum.R
+import com.example.comum.databinding.LoginFragmentBinding
 
 class LoginFragment : Fragment() {
+    lateinit var button : Button
 
     companion object {
         fun newInstance() = LoginFragment()
     }
 
     private lateinit var viewModel: LoginViewModel
+    private  lateinit var binding: LoginFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,9 +28,16 @@ class LoginFragment : Fragment() {
         return inflater.inflate(R.layout.login_fragment, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
+
+
+//        binding.buttonLogin.setOnClickListener {
+//
+//        }
+
+        //button_login
         // TODO: Use the ViewModel
     }
 
---
+++ comum/src/main/java/com/example/comum/presentation/ui/MenuViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.comum.presentation.ui
+
+import androidx.lifecycle.ViewModel
+
+class MenuViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
